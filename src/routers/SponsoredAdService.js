39f71ec67339a56f7c62b55c3bb390f289521d24
DELL <hirensorathiya577@gmial.com>
2024-04-21 21:13:49 +0530

    ->Solved issue old image not delete in folder

--- src/routers/SponsoredAdService.js
+++ src/routers/SponsoredAdService.js
@@ -8,7 +8,29 @@ const { getSponsoredAdService, uploadSponsoredData } = require("../controllers/S
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        return cb(null, "./uploads/SponsoredAd");
+        // Set the destination directory for file uploads
+        const uploadDir = "./uploads/SponsoredAd";
+
+        // Delete all files in the destination directory before uploading new files
+        fs.readdir(uploadDir, (err, files) => {
+            if (err) {
+                console.error("Error reading directory:", err);
+                return cb(err);
+            }
+
+            // Delete each file in the directory
+            for (const file of files) {
+                fs.unlink(path.join(uploadDir, file), err => {
+                    if (err) {
+                        console.error("Error deleting file:", err);
+                    }
+                });
+            }
+
+            // Proceed with the file upload after deleting all files
+            cb(null, uploadDir);
+            // return cb(null, "./uploads/SponsoredAd");
+        });
     },
 
     filename: function (req, file, cb) {
@@ -22,6 +44,6 @@ const uploadFile = multer({ storage: storage });
 
 router.route("/SponsoredAdService").post(upload.none(), getSponsoredAdService);
 router.route("/upload/SponsoredAdService").post(uploadFile.single('adMediaView'), uploadSponsoredData);
-router.use("/SponsoredAd",express.static('uploads/SponsoredAd'));
+router.use("/SponsoredAd", express.static('uploads/SponsoredAd'));
 
 module.exports = router;
