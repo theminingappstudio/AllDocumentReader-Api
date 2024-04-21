3103cbb70cb3d115b04153933724b4a19f4f7d60
DELL <hirensorathiya577@gmial.com>
2024-04-21 21:26:48 +0530

    ->Solved issue old image not delete in folder.

--- src/routers/SponsoredAdService.js
+++ src/routers/SponsoredAdService.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const multer = require("multer");
 const path = require('path')
+const fs = require('fs');
 const router = express.Router();
 const upload = multer();
 
